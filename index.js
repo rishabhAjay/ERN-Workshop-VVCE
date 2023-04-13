6ba5647fde092de9484e19c4d9d76e089e0ef505
rishabhAjay <rishabhajay24@gmail.com>
2023-04-14 01:36:49 +0530

    added routes to server-side session management

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
 import express from "express";
-import createConnection from "./config/db";
+
+import session from "express-session";
+
+import MongoStore from "connect-mongo";
+
+import cookieParser from "cookie-parser";
+import { mongoURL, sessionSecret } from "./utils/envVariables.js";
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["http://localhost:3000"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  res.append("Access-Control-Allow-Credentials", "true");
+  next();
+});
+app.use(cookieParser());
+const thirtySeconds = 1000 * 30;
 
-//create DB connection
-createConnection();
+app.use(
+  session({
+    secret: sessionSecret,
+    saveUninitialized: false,
+    cookie: { maxAge: thirtySeconds },
+    resave: true,
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: mongoURL,
+      dbName: "workshop-example-1",
+      autoRemove: "native",
+    }),
+  })
+);
 
+const myusername = "user";
+const mypassword = "password";
 //define routes
+app.post("/login", (req, res) => {
+  try {
+    if (req.body.username == myusername && req.body.password == mypassword) {
+      req.session.user = req.body.username;
+      console.log(req.session);
+      res.json({
+        data: `Hey there, welcome <a href=\'/logout'>click to logout</a>`,
+      });
+    } else {
+      res.json({
+        data: `Invalid data`,
+      });
+    }
+  } catch (error) {
+    console.log("Error: ", error.message);
+    res.json({
+      data: `Could not login`,
+    });
+  }
+});
+
+app.get("/user", (req, res) => {
+  try {
+    res.json({ data: req.session.user });
+  } catch (error) {
+    console.log("Error: ", error.message);
+    res.json({
+      data: `No active session`,
+    });
+  }
+});
+
+app.post("/logout", (req, res) => {
+  try {
+    req.session.destroy();
+    res.json({ data: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error: ", error.message);
+
+    res.json({
+      data: `Could not logout`,
+    });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
