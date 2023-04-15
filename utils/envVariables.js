e27fae0e054b51f9e48314ed168c02e36e52ac44
rishabhAjay <rishabhajay24@gmail.com>
2023-04-15 11:47:51 +0530

    code clean up and removed unused undependencies

--- utils/envVariables.js
+++ utils/envVariables.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const jwtSecret = process.env.JWT_SECRET;
+export const sessionSecret = process.env.SESSION_SECRET;
 export const mongoURL = process.env.MONGO_URL;
