0bd842c3ff6aba21badb4b78fed4a08ad89c556e
Rishabh Ajay <rishabh.a@spurtreetech.com>
2023-04-15 11:42:42 +0530

    added dependencies and initial boilerplate for jwt session management

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import express from "express";
-import createConnection from "./config/db";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-
-//create DB connection
-createConnection();
-
-//define routes
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
---
+++ middlewares/auth.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import { jwtSecret } from "../utils/envVariables.js";
+
+const auth = (req, res, next) => {
+  //get the token out of the request header
+};
+
+export default auth;
